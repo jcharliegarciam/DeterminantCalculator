f3e29ad72039a7ea0c6832a4c27ca2fdbb74e6d4
Carlos García <jcharliegmontes@gmail.com>
2018-07-22 02:21:16 -0500

    Goes throgh the matrix
    It creates all the sub matrixes to calculate the result

--- MatrixDeterminant.cpp
+++ MatrixDeterminant.cpp
@@ -24,26 +24,45 @@ void fillArray(int** array, int size){
 void printArray(int** array, int size){
 	for(int y = 0; y < size; y++){
 		for(int x = 0; x < size; x++){
-			cout << array[y][x] << endl;
+			cout << array[y][x] << " ";
 		}
-	}	
+		cout << endl;
+	}
+	cout << endl;
 }
-
 
 int determinant(int** matrix, int size){
 	if(size == 2){
 		return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
 	} else {
+		printArray(matrix, size);
 		for(int y = 0; y < size; y++){
-			int subMatrix[size - 1][size - 1] = {};
-			for(int sy = 0; sy < size; y++){
+			
+			int i = 0;
+			int *numbersForNewRecursiveCall = new int[(size - 1) * (size - 1)];
+			
+			for(int sy = 0; sy < size; sy++){
 				for(int sx = 0; sx < size; sx++){
-					if(sy != y && sx != y){
-						
+					if(sy != 0 && sx != y){
+						numbersForNewRecursiveCall[i] = matrix[sy][sx];
+						i++;
 					}
 				}
 			}
 			
+			
+			i = 0;
+			int **subMatrix = declareArray(size-1);
+			for(int sy = 0; sy < size-1; sy++){
+				for(int sx = 0; sx < size-1; sx++){
+					subMatrix[sy][sx] = numbersForNewRecursiveCall[i];
+					i++;
+				}
+			}
+			
+			printArray(subMatrix, size-1);
+			
+			determinant(subMatrix, size-1);
 		}
 		return -1;
 	}
@@ -52,10 +71,12 @@ int determinant(int** matrix, int size){
 int main(){
 	int arraySize = 0;
 	cout << "Cual desea que sea el tamaño de la matriz?_";
-	cin << arraySize <<;
+	cin >> arraySize;
 	int **matrix = declareArray(arraySize);
 	fillArray(matrix, arraySize);
+	
 	cout << determinant(matrix, arraySize) << endl;
+	
 	system("PAUSE");
 	return 0;
 }
